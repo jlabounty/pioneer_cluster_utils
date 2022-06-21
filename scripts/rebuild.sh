4092ba774cc8758fc4ef707e05435d92932b5161
Josh LaBounty <jjlab@uw.edu>
2022-06-20 23:21:06 -0700

    Adds shell script to rebuild / copy container to the cluster

---
+++ scripts/rebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+this_dir=`git rev-parse --show-toplevel`
+cd $this_dir
+cd containers
+
+echo git dir: $this_dir
+pwd
+ls -ltrh
+
+sudo singularity build python_analysis.sif python_analysis.def
+
+rsync -avh --progress ./python_analysis.sif rocks:/data/eliza2/g2/users/labounty/
